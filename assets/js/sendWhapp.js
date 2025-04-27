f8d82be4582278ee3d47e48e89e8ae212a3fe225
078JhonRamirez <120290317+078JhonRamirez@users.noreply.github.com>
2025-04-27 12:33:52 -0500

    actualizacion envio de mensaje whatsapp

--- assets/js/sendWhapp.js
+++ assets/js/sendWhapp.js
@@ -1,5 +1,5 @@
 const mensaje = "Hola, prueba de envio de mensaje desde la pagina web de Sandra"
-const phone = "3116264471"
+const phone = "573116264471"
 const urlWhapp = `https://api.whatsapp.com/send?phone=${phone}=&text=${mensaje}`;
 const activeButton = document.querySelector("#click__button");
 
